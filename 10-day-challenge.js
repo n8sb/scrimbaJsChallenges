802b4a279f5bff7067216c1213cc0c8dae7be01f
Nate Bennett <Nathaniel.bennett@gmail.com>
2020-06-28 00:20:56 -0700

    Create 10-day-challenge.js

---
+++ 10-day-challenge.js
@@ -0,0 +1,52 @@
+//Add array
+function add(param1, param2) {
+    //  write code here.
+    var arr = Array.from(arguments);
+    var total = arr.reduce((a, b) => a + b, 0)
+    return total;
+}
+
+//All the longest strings
+function allLongestStrings(array) {
+    //  write code here.
+  var arr = [];
+  arr.push(array[0]);
+  
+  for(let i = 1; i < array.length; i++){
+    if(arr.length === 1){
+      var len = 0;
+    }
+    else{
+      len = arr.length-1;
+    }
+    console.log(len);
+    if(arr[len].length === array[i].length){
+      console.log(arr);
+      arr.push(array[i]);
+    }
+    else if(arr[len].length < array[i].length){
+      arr = [];
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+}
+
+//Alternating sums
+function alternatingSums(array) {
+    //  write code here.
+    var arr = [];
+    var evenSum = 0;
+    var oddSum = 0;
+    for(var i=0; i < array.length; i++){  
+        if(i%2 === 0){
+          evenSum += array[i];
+        }
+        else {
+          oddSum += array[i];             
+        };
+      };
+    arr.push(evenSum);
+    arr.push(oddSum);
+    return arr;
+}
